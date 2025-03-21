a62f58ed7c345fef3f03e27fe3c8712a94f6d82a
Adam Florence <aflorence999@gmail.com>
2025-03-22 00:26:23 +1100

    move api and webapp to other repos

--- webapp/src/utils.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(timestamp) {
-    const date = new Date(timestamp);
-    return date.toLocaleString("en-GB", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-    });
-}
